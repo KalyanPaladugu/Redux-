9f9abc87537e45519831f26213466b3f149a7fd9
KalyanPaladugu <kalyanchakravarthi.1994@gmail.com>
2021-05-04 15:26:55 +0530

    Middleware

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const redux=require('redux');
+const reduxLogger=require('redux-logger')
 const createStore=redux.createStore;
 const combineReducers=redux.combineReducers
-
+const applyMiddleware=redux.applyMiddleware
+const logger=reduxLogger.createLogger()
 // Action
 const BUY_CAKE='BUY_CAKE'
 const BUY_ICECREAM='BUY_ICECREAME'
@@ -82,9 +84,9 @@ const rootReducer=combineReducers({
     cake:cakeReducer,
     iceCream:iceCreamReducer
 })
-const store= createStore(rootReducer)
+const store= createStore(rootReducer,applyMiddleware(logger))
 console.log("Initial Sate",store.getState())
-const unsubscribe=store.subscribe(()=> {console.log("Updated state",store.getState())})
+const unsubscribe=store.subscribe(()=> {})
 store.dispatch(buycake())
 store.dispatch(buycake())
 store.dispatch(buyIceCreame())
